39281922e312a97843ac8cc28a5976e2d3550ff0
mahcodes <mhmdali102@bk.ru>
2023-03-17 19:59:00 +0200

    feat: hide navbar on scroll down

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { logo, title } from "@/ublog.config";
 import Link from "next/link";
 import Login from "./Login";
@@ -9,25 +9,51 @@ import Relays from "./Relays";
 
 interface HeaderProps {}
 
-const Header: FC<HeaderProps> = () => (
-  <nav className="navbar bg-base-100 sticky top-0 z-30">
-    <div className="flex-1">
-      <Link href="/" className="font-bold text-xl flex gap-2">
-        {logo ? (
-          <div className="w-8 rounded-full bg-white">
-            <img src={logo} alt="" />
-          </div>
-        ) : null}
-        {title ? <span>{title}</span> : null}
-      </Link>
-    </div>
-    <div className="flex items-center gap-2">
-      <Relays />
-      <Write />
-      <Themes />
-      <Login />
-    </div>
-  </nav>
-);
+const Header: FC<HeaderProps> = () => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    let prevY = 0;
+
+    const handleScroll = () => {
+      const currentY = window.scrollY;
+      if (currentY <= 150 || currentY < prevY) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+      prevY = currentY;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={`navbar bg-base-100 sticky z-30 top-0 transition-transform ${
+        visible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <div className="flex-1">
+        <Link href="/" className="font-bold text-xl flex gap-2">
+          {logo ? (
+            <div className="w-8 rounded-full bg-white">
+              <img src={logo} alt="" />
+            </div>
+          ) : null}
+          {title ? <span>{title}</span> : null}
+        </Link>
+      </div>
+      <div className="flex items-center gap-2">
+        <Relays />
+        <Write />
+        <Themes />
+        <Login />
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
