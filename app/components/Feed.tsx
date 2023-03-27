fc034ff177fa6b225879244e256aa6ca0c562a1a
mahcodes <mhmdali102@bk.ru>
2023-03-28 01:00:55 +0300

    feat: add no blogs state

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -2,6 +2,7 @@ import type { Event } from "nostr-tools";
 import { FC } from "react";
 import Article from "@/app/components/Article";
 import ArticleSkeleton from "@/app/components/Skeleton/Article";
+import NoBlogs from "@/app/components/NoBlogs";
 
 interface FeedProps {
   events: Event[];
@@ -9,13 +10,16 @@ interface FeedProps {
 }
 
 const Feed: FC<FeedProps> = ({ events, isEventsLoading }) => {
+
   return (
     <div className="flex flex-col gap-4 py-4">
-      {isEventsLoading
-        ? Array.from({ length: 4 }).map((_, i) => <ArticleSkeleton key={i} />)
-        : events.map((event: Event) => (
-            <Article key={event.id} event={event} />
-          ))}
+      {isEventsLoading ? (
+        Array.from({ length: 3 }).map((_, i) => <ArticleSkeleton key={i} />)
+      ) : events.length === 0 ? (
+        <NoBlogs />
+      ) : (
+        events.map((event: Event) => <Article key={event.id} event={event} />)
+      )}
     </div>
   );
 };
